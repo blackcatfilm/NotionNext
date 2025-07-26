70f023c2b525657de253361c7f5b19edcba58afa
blackcatfilm <165353378+blackcatfilm@users.noreply.github.com>
2025-07-26 12:37:56 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -8,10 +8,10 @@ export default function PoweredBy(props) {
   return (
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
-     © 2022–2025 <a
+      <a
         href='https://blackcatfilm.cc'
         className='underline justify-start'>
-        Blackcatfilm 黑猫Emby媒体库
+        黑猫Emby媒体库
       </a>
       .
     </div>
